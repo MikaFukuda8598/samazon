bf3b3ed63a68d95024adb6b3b8828d548b3c27a2
MikaFukuda8598 <mikg4505@gmail.com>
2021-03-09 00:07:19 +0000

    edit posts view

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 2021_03_03_174059) do
 
   create_table "categories", force: :cascade do |t|
     t.string "major_category_name"
-    t.string "name"
-    t.text "description"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
